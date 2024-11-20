75e18902b8c44eaa0028e08265396987f317efdf
StamFrank <eugenii.strunets@gmail.com>
2024-11-20 21:28:44 +0300

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,29 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Android Lesson"
+include(":app")
+include(":p0031-1firstproject")
+include(":p0041basicviews")
+include(":p0041-1basicviews")
+include(":p0041-2basicviews")
+include(":p0051layoutfiles")
+include(":p00511layoutfiles")
